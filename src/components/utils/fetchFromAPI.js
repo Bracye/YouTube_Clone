ef2897f50fef8699fd3b7575a035ea203f7874ca
Ernest Bracy <ernestbracy1@gmail.com>
2023-07-24 09:29:42 -0400

    Finished all components, functionality, and fetching api to create clone app

---
+++ src/components/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+export const BASE_URL = "https://youtube-v31.p.rapidapi.com";
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    "X-RapidAPI-Key": "48ecb2c10amsh699aa641a9a45b3p13bc01jsn1ae890ade1ad",
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
